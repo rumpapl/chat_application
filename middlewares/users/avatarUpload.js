e0f2d86e2ca4209903b62963fce240e7c8df8470
rumpapl <rumpakucse14@gmail.com>
2021-09-14 16:49:01 +0600

    worked on single-file-upload part using [MULTER]

---
+++ middlewares/users/avatarUpload.js
@@ -0,0 +1,27 @@
+// internal imports
+const uploader = require("../../utilities/singleFileUploder");
+
+function avatarUpload(req, res, next) {
+  const upload = uploader(
+    "avatars",
+    ["image/jpeg", "image/jpg", "image/npg"],
+    1000000,
+    "Only .jpg, .jpeg or .png format allowed!"
+  );
+
+  // call the middleware function
+  upload.any()(req, res, (err) => {
+    if (err) {
+      res.status(500).json({
+        errors: {
+          avatar: {
+            msg: err.message,
+          },
+        },
+      });
+    } else {
+      next();
+    }
+  });
+}
+module.exports = avatarUpload;
