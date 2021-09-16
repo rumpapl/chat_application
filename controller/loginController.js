8f364b1f2221bf10a0fc7a3f473ac7e5ccf94722
rumpapl <rumpakucse14@gmail.com>
2021-09-16 11:59:11 +0600

    worked on user-login

--- controller/loginController.js
+++ controller/loginController.js
@@ -1,9 +1,76 @@
-// get login page
+// external imports
+const bcrypt = require("bcrypt");
+const createError = require("http-errors");
+const jwt = require("jsonwebtoken");
 
+// internal imports
+const Users = require("../models/People");
+
+// get login page
 function getLogin(req, res, next) {
   res.render("index");
 }
 
+// do login
+async function login(req, res, next) {
+  try {
+    // fine a user who has this email/mobile no.
+    const user = await Users.findOne({
+      $or: [{ email: req.body.username }, { mobile: req.body.username }],
+    });
+
+    if (user && user._id) {
+      // compare the password with DB-password
+      const isValidPassword = await bcrypt.compare(
+        req.body.password,
+        user.password
+      );
+
+      if (isValidPassword) {
+        // prepare the user object to generate token
+        const userObject = {
+          username: user.name,
+          mobile: user.mobile,
+          email: user.email,
+          role: "user",
+        };
+
+        // generate token
+        const token = jwt.sign(userObject, process.env.JWT_SECRET, {
+          expiresIn: process.env.JWT_EXPIRY,
+        });
+
+        // set cookie
+        res.cookie(process.env.COOKIE_NAME, token, {
+          maxAge: process.env.JWT_EXPIRY,
+          httpOnly: true,
+          signed: true,
+        });
+
+        // set logged in user local identifier
+        res.locals.loggedInUser = userObject;
+        res.render("inbox");
+      } else {
+        throw createError("Login failed! Please try again.");
+      }
+    } else {
+      throw createError("Login failed! Please try again.");
+    }
+  } catch (err) {
+    res.render("index", {
+      data: {
+        username: req.body.username,
+      },
+      errors: {
+        common: {
+          msg: err.message,
+        },
+      },
+    });
+  }
+}
+
 module.exports = {
   getLogin,
+  login,
 };
--- router/loginRouter.js
+++ router/loginRouter.js
@@ -2,13 +2,25 @@
 const express = require("express");
 
 // internal imports
-
-const { getLogin } = require("../controller/loginController");
+const { getLogin, login } = require("../controller/loginController");
 const decorateHtmlResponse = require("../middlewares/common/decorateHtmlResponse");
+const {
+  addLoginValidator,
+  addLoginValidationHandler,
+} = require("../middlewares/login/loginValidator");
 
 const router = express.Router();
 
 // login page
 router.get("/", decorateHtmlResponse("Login"), getLogin);
+
+// process login
+router.post(
+  "/",
+  decorateHtmlResponse("Login"),
+  addLoginValidator,
+  addLoginValidationHandler,
+  login
+);
 
 module.exports = router;
